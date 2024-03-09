970293271ec2fe1c680971bd11f2047991b96291
EthanTravers <et2g21@soton.ac.uk>
2024-03-09 21:43:57 +0000

    golem spawn x3

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -70,13 +70,24 @@ live_warden_spawn = "summon warden -113 130 370"
 threading.Thread(target=play_audio).start()
 
 
-def zombie_spawn():
+def live_zombie_spawn():
     headers = {
         'accept': '*/*',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
     data = {
         'command': live_zombie_spawn,
+        'time': ''
+    }
+    response = requests.post(command_url, headers=headers, data=data)
+    print(response)
+def live_golem_spawn():
+    headers = {
+        'accept': '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'command': live_golem_spawn,
         'time': ''
     }
     response = requests.post(command_url, headers=headers, data=data)
@@ -89,24 +100,17 @@ while chat.is_alive():
         message = c.message
         print(f"{c.datetime} [{c.author.name}]- {c.message}")
         if message.lower() == "golem":
-            headers = {
-                'accept': '*/*',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-            data = {
-                'command': live_golem_spawn,
-                'time': ''
-            }
-            response = requests.post(command_url, headers=headers, data=data)
-            print(response)
+            live_golem_spawn()
+            live_golem_spawn()
+            live_golem_spawn()
             thread = threading.Thread(target=text_to_speech, args=(message,))
             thread.start()
         elif message.lower() == "zombie":
-            zombie_spawn()
-            zombie_spawn()
-            zombie_spawn()
-            zombie_spawn()
-            zombie_spawn()
+            live_zombie_spawn()
+            live_zombie_spawn()
+            live_zombie_spawn()
+            live_zombie_spawn()
+            live_zombie_spawn()
             thread = threading.Thread(target=text_to_speech, args=(message,))
             thread.start()
         elif message.lower() == "tnt":
